70ef8694d8d537e0d693d3027a505380f4bd4e29
鏡　桂一郎 <k022G0120@g.neec.ac.jp>
2024-02-17 03:12:55 +0900

    プロジェクト ファイルを追加します。

---
+++ Enemy.h
@@ -0,0 +1,32 @@
+#pragma once
+class Enemy
+{
+public:
+	//行動フェーズ
+	enum class Phase {
+		Approach,//接近
+		Leave,//離脱
+	};
+
+	void Update();
+
+	Phase GetPhase() { return phase_; }
+	void SetPhase(Phase phase) { phase_ = phase; }
+
+private:
+	//接近フェーズ更新
+	void ApproachUpdate();
+
+	//離脱フェーズ更新
+	void LeaveUpdate();
+
+private:
+	//メンバ関数ポインタ
+	void (Enemy::* pFunc)();
+
+	//メンバ関数ポインタのテーブル
+	static void (Enemy::* spFuncTable[])();
+
+	//フェーズ
+	Phase phase_ = Phase::Approach;
+};
---
+++ MyMath.h
@@ -0,0 +1,21 @@
+#pragma once
+template <typename T1, typename T2>
+
+class MyMath{
+public:
+	T1 a;
+	T2 b;
+
+	MyMath(T1 a, T2 b) : a(a), b(b) {}
+
+	T1 Min();
+};
+
+template<typename T1, typename T2>
+inline T1 MyMath<T1, T2>::Min(){
+	if (a >= b) {
+		return static_cast<T1>(a);
+	} else if (a < b) {
+		return static_cast<T1>(b);
+	}
+}
